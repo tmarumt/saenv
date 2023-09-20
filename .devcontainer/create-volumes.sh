b740b158026b32fa846cc759f430609236233d2c
tmarumt <tmarumt1@gmail.com>
2023-09-20 13:17:51 +0900

    Add and apply indent rule of shellscript

--- .devcontainer/create-volumes.sh
+++ .devcontainer/create-volumes.sh
@@ -3,7 +3,7 @@
 volumes=("renv" "pip" "julia" "TinyTeX" "fonts")
 
 for vol in ${volumes[@]}; do
-    if [ ! "$(docker volume ls -q -f name="$vol")" ]; then
-        docker volume create $vol
-    fi
+  if [ ! "$(docker volume ls -q -f name="$vol")" ]; then
+    docker volume create $vol
+  fi
 done
