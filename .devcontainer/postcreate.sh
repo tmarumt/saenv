a11325fad2f1853866056d70e52c493e2c1d20ec
tmarumt <tmarumt1@gmail.com>
2024-03-28 23:29:01 +0900

    fix: improve if statements

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ -z "$(ls /home/rstudio/.TinyTeX)" ]; then
+if [ -z "$(ls -A /home/rstudio/.TinyTeX)" ]; then
   Rscript -e 'tinytex::install_tinytex(dir = "/home/rstudio/.TinyTeX", force = TRUE)'
 fi
 
-if [ -z "$(ls /home/rstudio/.pip)" ]; then
+if [ ! -e /home/rstudio/.pip/bin/dvc ]; then
   pip install --user dvc dvc-gdrive
 fi
